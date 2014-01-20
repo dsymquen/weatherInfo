35e00e6e5abce7610b6c368e95765579453ca7e4
dsymquen <Dsymquen@gmail.com>
2014-01-19 21:46:36 -0500

    adding lat/long and url

--- geo.js
+++ geo.js
@@ -1,5 +1,11 @@
 (function(){
 
-    navigator.geolocation.getCurrentPosition(function(P){console.log(P.coords)});
+    navigator.geolocation.getCurrentPosition(function(P){
+        
+        console.log(P.coords.latitude);
+        console.log(P.coords.longitude);
+        var url = 'http://forecast.weather.gov/MapClick.php?lat='+P.coords.latitude+'&lon='+P.coords.latitude+'&unit=0&lg=english&FcstType=dwml';
+        console.log(url);
+    });
 
 })();
