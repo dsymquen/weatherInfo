1bacef9341cd1b19efa9ff41f06c6acaf50fa42e
dsymquen <Dsymquen@gmail.com>
2014-01-19 22:39:05 -0500

    adding json link

--- geo.js
+++ geo.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         console.log(P.coords.latitude);
         console.log(P.coords.longitude);
         var url = 'http://forecast.weather.gov/MapClick.php?lat='+P.coords.latitude+'&lon='+P.coords.longitude+'&unit=0&lg=english&FcstType=dwml';
+        //http://forecast.weather.gov/MapClick.php?lat=40.52880&lon=-74.3693&unit=0&lg=english&FcstType=json
         console.log(url);
         $.get(url,function(d){
             $('section').html(d);
